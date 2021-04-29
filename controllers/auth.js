93bbe7f605e056a4766eaca5686bd7054063f2d5
ahmed gamal <agemy844@gmail.com>
2021-04-29 15:11:11 +0200

    added register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,35 @@
 const pool = require("../config/db");
-
+const bcrypt = require("bcrypt");
+const { listen } = require("../routes/todos");
 
 //TODO HANDLE THE  register request
 
 
 exports.register = async(req , res , next)=>{
 
-    res.send("hello from register auth route");
+    const {userName , userEmail } = req.body;
+    let {userPassword} = req.body;
+
+    if(!userName || !userEmail || !userPassword){
+        return res.status(400).json({ message: 'Bad request'});
+    }
+
+    let user = await pool.query('select * from "public"."user" where "userEmail" = $1',[userEmail]);
+
+    if(user.rows.length > 0){
+        return res.status(400).json({ message: 'This email had registered already !' });
+    }
+
+    const salt = await bcrypt.genSalt(15);
+
+    userPassword = await bcrypt.hash(userPassword , salt);
+
+    user = await pool.query('INSERT INTO "public"."user" ( "userName", "userEmail", "userPassword")  VALUES ( $1, $2, $3 ) RETURNING *;',[userName,userEmail,userPassword]);
+
+    if(!user){
+        return res.status(500).json({ message: 'SERVER ERROR' });
+    }
+    return res.json({success : true, data : user.rows});
 
 }
 
--- database/database.sql
+++ database/database.sql
@@ -7,3 +7,18 @@ CREATE TABLE todo(
     describtion VARCHAR(225)
 );
 
+--creating the users data base 
+
+   
+CREATE TABLE user ( 
+	user_id Character Varying( 150 ) DEFAULT REPLACE(gen_random_uuid()::text,'-','') NOT NULL,
+	userName Character Varying( 50 ) NOT NULL,
+	userEmail Character Varying( 150 ) NOT NULL,
+	userPassword Character Varying( 150 ) NOT NULL,
+    PRIMARY KEY ( user_id ),
+	CONSTRAINT unique_user_user_id UNIQUE( "user_id" ),
+	CONSTRAINT unique_user_userEmail UNIQUE( "userEmail" ) );
+
+
+--INSERT INTO "public"."user" ( "userName", "userEmail", "userPassword") 
+--VALUES ( <userName>, <userEmail>, <userPassword> );
